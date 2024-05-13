930326745e0c50a8f72be2979c007fd1867f29d7
Arunbalajiyoho <140370898+Arunbalajiyoho@users.noreply.github.com>
2024-05-13 20:53:11 +0530

    arunbalaji commit

--- src/features/student-pages/help-center-page/components/HelpCenterView.js
+++ src/features/student-pages/help-center-page/components/HelpCenterView.js
@@ -5,16 +5,16 @@ const HelpCenterView = () => {
   return (
     <>
       <Grid container spacing={3} xs={12}>
-        <Grid item sx={12} md={7}>
+        <Grid item xs={12} md={7}>
           <Box>
             <Box>
               <Typography variant="h4">
-                {" "}
-                Explaining design to someone who doesn't understand what it is ??
+                Explaining design to someone who doesn't understand what it is
+                ??
               </Typography>
             </Box>
             <Box sx={{ marginTop: 4 }}>
-              <Typography variant="body1">
+              <Typography variant="body1" sx={{ textAlign: "justify" }}>
                 Lorem Ipsum is simply dummy text of the printing and typesetting
                 industry. Lorem Ipsum has been the industry's standard dummy
                 text ever since the 1500s, when an unknown printer took a galley
@@ -23,10 +23,36 @@ const HelpCenterView = () => {
                 electronic typesetting, remaining essentially unchanged.
               </Typography>
             </Box>
+            <Box sx={{ marginTop: 3 }}>
+              <Typography variant="h4">Step by Step</Typography>
+              <ul style={{ marginLeft: "1.5rem" }}>
+                <li style={{ marginBottom: "1rem" }}>
+                  Step 1: Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry.
+                </li>
+                <li style={{ marginBottom: "1rem" }}>
+                  Step 2: Lorem Ipsum has been the industry's standard dummy
+                  text ever since the 1500s.
+                </li>
+                <li style={{ marginBottom: "1rem" }}>
+                  Step 3: When an unknown printer took a galley of type and
+                  scrambled it to make a type specimen book.
+                </li>
+                <li style={{ marginBottom: "1rem" }}>
+                  Step 4: It has survived not only five centuries, but also the
+                  leap into electronic typesetting, remaining essentially
+                  unchanged.
+                </li>
+                <li style={{ marginBottom: "1rem" }}>
+                  Step 5: It was popularised in the 1960s with the release of
+                  Letraset sheets containing Lorem Ipsum passages.
+                </li>
+              </ul>
+            </Box>
           </Box>
         </Grid>
 
-        <Grid item sx={12} md={5}>
+        <Grid item xs={12} md={5}>
           <Box>
             <Typography variant="h4">Video Guide</Typography>
           </Box>
