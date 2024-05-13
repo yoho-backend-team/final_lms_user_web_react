741ad96f745add6b7c6c3a451a44351a220a9c1d
prasanthyoho <159232197+prasanthyoho@users.noreply.github.com>
2024-05-13 05:36:13 +0530

    prasanth commit

--- src/views/student-pages/create-ticket-page/index.js
+++ src/views/student-pages/create-ticket-page/index.js
@@ -1,95 +1,32 @@
-import React, { useState } from 'react';
-import { makeStyles } from '@mui/material';
-import logo from '../../../assets/images/pages/auth-v2-forgot-password-illustration-light.png';
-import { Container, Paper, Typography, TextField, Button, InputLabel, Select, MenuItem, Dialog, DialogActions, DialogContent, DialogContentText } from '@mui/material';
-import CloudUploadIcon from '@mui/icons-material/CloudUpload';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    backgroundColor: '#F8F7FA',
-    padding: theme.spacing(4),
-    minHeight: '100vh',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  paper: {
-    padding: theme.spacing(2),
-    marginBottom: theme.spacing(2),
-    width: '100%',
-    position: 'relative',
-  },
-  header: {
-    textAlign: 'left',
-    marginBottom: theme.spacing(2),
-    fontWeight: 'bold',
-    fontSize: '15px',
-    color: 'black',
-  },
-  logo: {
-    width: '140px',
-    height: 'auto',
-    position: 'absolute',
-    top: '40%',
-    right: '40px',
-    transform: 'translateY(-50%)',
-  },
-  buttons: {
-    textAlign: 'right',
-    marginTop: theme.spacing(15),  
-  },
-  cancelButton: {
-    backgroundColor: '#F8F7FA',
-    color: 'black',
-    marginRight: theme.spacing(2),
-    '&:hover': {
-      boxShadow: `0 0 10px #F8F7FA`,
-    }
-  },  
-  confirmButton: {
-    textTransform: 'none', 
-    '&:hover': {
-      boxShadow: `0 0 10px ${theme.palette.primary.main}`,
-    },
-  },  
-  formControl: {
-    width: '100%',
-    marginTop: theme.spacing(3), 
-    marginBottom: theme.spacing(2),      
-  },
-  dialog: {
-    padding: '5px',
-    width: '360px', 
-    height:'180px',
-  },
-  contentText: {
-    color: 'black',
-    textAlign: 'center',    
-  },
-  button: {
-    marginTop:'-20px',
-    border: '1px solid #0043b0',
-    borderRadius: '30px',
-    width: '100px',
-    height: '10px',
-    fontSize: '12px',
-    marginRight: '12px',
-    color: '#0043b0',
-    '&:hover': {
-      color: 'white',
-      backgroundColor: '#005cf2', 
-    },
-  },
-}));
-
+import React, { useState } from "react";
+import logo from "../../../assets/images/pages/auth-v2-forgot-password-illustration-light.png";
+import {
+  Container,
+  Paper,
+  Typography,
+  TextField,
+  Button,
+  InputLabel,
+  Select,
+  MenuItem,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  useTheme,
+} from "@mui/material";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
+import Box from "@mui/material/Box";
+import useMediaQuery from "@mui/material/useMediaQuery";
 const CreateTicketPage = () => {
-  const classes = useStyles();
-  const [selectedProblem, setSelectedProblem] = useState('');
-  const [description, setDescription] = useState('');
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+  const [selectedProblem, setSelectedProblem] = useState("");
+  const [description, setDescription] = useState("");
   const [openCancelDialog, setOpenCancelDialog] = useState(false);
 
-  const handleFileUpload = () => {   
-  };
+  const handleFileUpload = () => {};
 
   const handleCancel = () => {
     setOpenCancelDialog(true);
@@ -100,93 +37,224 @@ const CreateTicketPage = () => {
   };
 
   return (
-    <div className={classes.root}>
-      <Container maxWidth="md">
-        <Paper className={classes.paper}>
-          <Typography variant="h6" className={classes.header}>Create Ticket for Your Problem</Typography>
-          <div className={classes.formControl}>
+    <div
+      style={{
+        backgroundColor: "#F8F7FA",
+        padding: "16px",
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Container>
+        <Paper
+          style={{
+            padding: "16px",
+            marginBottom: "16px",
+            position: "relative",
+            paddingLeft: "40px",
+            paddingRight: "40px",
+          }}
+        >
+          <Typography
+            variant="h6"
+            style={{
+              textAlign: "left",
+              marginTop: "16px",
+              fontWeight: "bold",
+              fontSize: "15px",
+              color: "black",
+            }}
+          >
+            Create Ticket for Your Problem
+          </Typography>
+          <div
+            style={{ width: "100%", marginTop: "24px", marginBottom: "16px" }}
+          >
             <InputLabel htmlFor="dropdown">Select your Problem</InputLabel>
             <Select
               id="dropdown"
               value={selectedProblem}
-              style={{ width: '230px', height: '40px', display: 'flex', alignItems: 'center', justifyContent: 'center', background:'#F8F7FA', color:'black'}}
-              onChange={(e) => setSelectedProblem(e.target.value)} 
+              style={{
+                width: "230px",
+                height: "40px",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                background: "#F8F7FA",
+                color: "black",
+              }}
+              onChange={(e) => setSelectedProblem(e.target.value)}
             >
               <MenuItem value={1}>Attendance issue</MenuItem>
               <MenuItem value={2}>Issue 2</MenuItem>
               <MenuItem value={3}>Issue 3</MenuItem>
             </Select>
           </div>
-          <div className={classes.formControl}>
+          <div
+            style={{ width: "100%", marginTop: "16px", marginBottom: "16px" }}
+          >
             <InputLabel htmlFor="description">Description</InputLabel>
             <TextField
               id="description"
               multiline
               rows={4}
-              style={{ width: '70%', height: 'auto', display: 'flex', justifyContent: 'center', background:'#F8F7FA', color:'black'}}
+              style={{
+                width: "70%",
+                height: "auto",
+                display: "flex",
+                justifyContent: "center",
+                background: "#F8F7FA",
+                color: "black",
+              }}
               value={description}
-              onChange={(e) => setDescription(e.target.value)} 
+              onChange={(e) => setDescription(e.target.value)}
             />
           </div>
-          <div className={classes.formControl}>
+          <div
+            style={{ width: "100%", marginTop: "16px", marginBottom: "16px" }}
+          >
             <InputLabel htmlFor="attachment">Attachment</InputLabel>
             <Button
               component="label"
               variant="contained"
               tabIndex={-1}
-              style={{ width: '230px', height: 'auto', display: 'flex', alignItems: 'center', justifyContent: 'center', background:'#d7e4f7', color:'#2c74e8'}}
+              style={{
+                width: "230px",
+                height: "auto",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                background: "#d7e4f7",
+                color: "#2c74e8",
+              }}
             >
-              <CloudUploadIcon style={{ fontSize: 24, marginRight: '5px' }} />
+              <CloudUploadIcon style={{ fontSize: 24, marginRight: "5px" }} />
               Upload
-              <input type="file" id="attachment" onChange={handleFileUpload} style={{ display: 'none' }} />
+              <input
+                type="file"
+                id="attachment"
+                onChange={handleFileUpload}
+                style={{ display: "none" }}
+              />
             </Button>
           </div>
-          <img src={logo} alt="Logo" className={classes.logo} />
-          <div className={classes.buttons}>
-            <Button
-              variant="contained"
-              color="secondary"
-              className={classes.cancelButton}
-              onClick={handleCancel}
-            >
-              Cancel
-            </Button>
-            <Button
-              variant="contained"
-              color="primary"
-              className={classes.confirmButton}
-            >
-              Confirm Ticket
-            </Button>
+          {/* <img src={logo} alt="Logo" style={{display:{ xs: 'none', sm: 'block' }, width: '140px', height: 'auto', position: 'absolute', top: '40%', right: '40px', transform: 'translateY(-50%)' }} /> */}
+          <img
+            src={logo}
+            alt="Logo"
+            style={{
+              display: isMobile ? "none" : "block",
+              width: "140px",
+              height: "auto",
+              position: "absolute",
+              top: "40%",
+              right: "40px",
+              transform: "translateY(-50%)",
+            }}
+          />
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              paddingTop: "50px",
+            }}
+          >
+            <Box sx={{ display: { xs: "none", sm: "block" } }}>
+              <Box sx={{ display: "flex", alignItems: "center" }}>
+                <ErrorOutlineIcon style={{ color: "blue" }} />
+                <Typography sx={{ marginLeft: "8px" }} color="blue">
+                  How it works ?
+                </Typography>
+              </Box>
+            </Box>
+            <div style={{ marginLeft: "auto" }}>
+              <Button
+                variant="contained"
+                color="secondary"
+                style={{
+                  backgroundColor: "#F8F7FA",
+                  color: "black",
+                  marginRight: "8px",
+                  "&:hover": { boxShadow: `0 0 10px #F8F7FA` },
+                }}
+                onClick={handleCancel}
+              >
+                Cancel
+              </Button>
+              <Button
+                variant="contained"
+                color="primary"
+                style={{
+                  textTransform: "none",
+                  "&:hover": {
+                    boxShadow: `0 0 10px ${theme.palette.primary.main}`,
+                  },
+                }}
+              >
+                Confirm Ticket
+              </Button>
+            </div>
           </div>
         </Paper>
       </Container>
 
-      
       <Dialog
         open={openCancelDialog}
         onClose={handleCloseCancelDialog}
         aria-labelledby="cancel-dialog-title"
         aria-describedby="cancel-dialog-description"
-        classes={{ paper: classes.dialog }}
+        style={{ padding: "5px" }}
       >
-        <DialogContent>
-          <DialogContentText className={classes.contentText} id="cancel-dialog-description" >
-              Do you Want to discard?
+        <DialogContent style={{ textAlign: "center" }}>
+          <DialogContentText
+            style={{ color: "black" }}
+            id="cancel-dialog-description"
+          >
+            Do you want to discard?
           </DialogContentText>
         </DialogContent>
-        <DialogActions style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-          <Button onClick={handleCloseCancelDialog} className={classes.button}>
+        <DialogActions style={{ justifyContent: "center" }}>
+          <Button
+            onClick={handleCloseCancelDialog}
+            style={{
+              border: "1px solid #0043b0",
+              borderRadius: "30px",
+              width: "100px",
+              height: "30px",
+              fontSize: "12px",
+              marginRight: "12px",
+              color: "#0043b0",
+              "&:hover": { color: "white", backgroundColor: "#005cf2" },
+            }}
+          >
             Cancel
           </Button>
-          <Button onClick={handleCloseCancelDialog} className={classes.button} autoFocus>
+          <Button
+            onClick={handleCloseCancelDialog}
+            style={{
+              border: "1px solid #0043b0",
+              borderRadius: "30px",
+              width: "100px",
+              height: "30px",
+              fontSize: "12px",
+              marginRight: "12px",
+              color: "#FFFFFF",
+              backgroundColor: "#005cf2",
+              "&:hover": {
+                color: "#000000",
+                backgroundColor: "#005cf2",
+              },
+            }}
+            autoFocus
+          >
             Discard
           </Button>
         </DialogActions>
       </Dialog>
-
     </div>
   );
-}
+};
 
 export default CreateTicketPage;
--- src/views/student-pages/tickets-page/index.js
+++ src/views/student-pages/tickets-page/index.js
@@ -1,260 +1,54 @@
-import React, { useState } from "react";
-import logo from "../../../assets/images/pages/auth-v2-forgot-password-illustration-light.png";
-import {
-  Container,
-  Paper,
-  Typography,
-  TextField,
-  Button,
-  InputLabel,
-  Select,
-  MenuItem,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-  useTheme,
-} from "@mui/material";
-import CloudUploadIcon from "@mui/icons-material/CloudUpload";
-import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
+import * as React from "react";
 import Box from "@mui/material/Box";
-import useMediaQuery from "@mui/material/useMediaQuery";
-const Tickets = () => {
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
-  const [selectedProblem, setSelectedProblem] = useState("");
-  const [description, setDescription] = useState("");
-  const [openCancelDialog, setOpenCancelDialog] = useState(false);
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+import { Grid, Typography } from "@mui/material";
+import Button from '@mui/material/Button';
+import TicketsCard from "features/student-pages/tickets-page/components/TicketsCard";
 
-  const handleFileUpload = () => {};
+const TicketsPage = () => {
+  const [value, setValue] = React.useState("1");
 
-  const handleCancel = () => {
-    setOpenCancelDialog(true);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
   };
-
-  const handleCloseCancelDialog = () => {
-    setOpenCancelDialog(false);
-  };
-
   return (
-    <div
-      style={{
-        backgroundColor: "#F8F7FA",
-        padding: "16px",
-        minHeight: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Container>
-        <Paper
-          style={{
-            padding: "16px",
-            marginBottom: "16px",
-            position: "relative",
-            paddingLeft: "40px",
-            paddingRight: "40px",
-          }}
-        >
-          <Typography
-            variant="h6"
-            style={{
-              textAlign: "left",
-              marginTop: "16px",
-              fontWeight: "bold",
-              fontSize: "15px",
-              color: "black",
-            }}
-          >
-            Create Ticket for Your Problem
-          </Typography>
-          <div
-            style={{ width: "100%", marginTop: "24px", marginBottom: "16px" }}
-          >
-            <InputLabel htmlFor="dropdown">Select your Problem</InputLabel>
-            <Select
-              id="dropdown"
-              value={selectedProblem}
-              style={{
-                width: "230px",
-                height: "40px",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                background: "#F8F7FA",
-                color: "black",
-              }}
-              onChange={(e) => setSelectedProblem(e.target.value)}
-            >
-              <MenuItem value={1}>Attendance issue</MenuItem>
-              <MenuItem value={2}>Issue 2</MenuItem>
-              <MenuItem value={3}>Issue 3</MenuItem>
-            </Select>
-          </div>
-          <div
-            style={{ width: "100%", marginTop: "16px", marginBottom: "16px" }}
-          >
-            <InputLabel htmlFor="description">Description</InputLabel>
-            <TextField
-              id="description"
-              multiline
-              rows={4}
-              style={{
-                width: "70%",
-                height: "auto",
-                display: "flex",
-                justifyContent: "center",
-                background: "#F8F7FA",
-                color: "black",
-              }}
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-            />
-          </div>
-          <div
-            style={{ width: "100%", marginTop: "16px", marginBottom: "16px" }}
-          >
-            <InputLabel htmlFor="attachment">Attachment</InputLabel>
-            <Button
-              component="label"
-              variant="contained"
-              tabIndex={-1}
-              style={{
-                width: "230px",
-                height: "auto",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                background: "#d7e4f7",
-                color: "#2c74e8",
-              }}
-            >
-              <CloudUploadIcon style={{ fontSize: 24, marginRight: "5px" }} />
-              Upload
-              <input
-                type="file"
-                id="attachment"
-                onChange={handleFileUpload}
-                style={{ display: "none" }}
-              />
-            </Button>
-          </div>
-          {/* <img src={logo} alt="Logo" style={{display:{ xs: 'none', sm: 'block' }, width: '140px', height: 'auto', position: 'absolute', top: '40%', right: '40px', transform: 'translateY(-50%)' }} /> */}
-          <img
-            src={logo}
-            alt="Logo"
-            style={{
-              display: isMobile ? "none" : "block",
-              width: "140px",
-              height: "auto",
-              position: "absolute",
-              top: "40%",
-              right: "40px",
-              transform: "translateY(-50%)",
-            }}
-          />
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              paddingTop: "50px",
-            }}
-          >
-            <Box sx={{ display: { xs: "none", sm: "block" } }}>
-              <Box sx={{ display: "flex", alignItems: "center" }}>
-                <ErrorOutlineIcon style={{ color: "blue" }} />
-                <Typography sx={{ marginLeft: "8px" }} color="blue">
-                  How it works ?
-                </Typography>
-              </Box>
+    <Box sx={{m:4}}>
+    <Grid container spacing={3} sx={{p:2,alignItems:'flex-start'}}>
+      <Grid item md={1} xs={12} sx={{mt:2}}>
+        <Typography variant="h2">Ticket</Typography>
+      </Grid>
+      <Grid item md={9} xs={12}>
+        {/* <Box sx={{display:"flex",justifyContent:"space-between",alignItems:"flex-start"}}> */}
+          <Grid sx={{  typography: "body1" }}>
+          <TabContext value={value}>
+            <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+              <TabList
+                onChange={handleChange}
+                aria-label="lab API tabs example"
+              >
+                <Tab label="All" value="1" />
+                <Tab label="Open" value="2" />
+                <Tab label="Close" value="3" />
+              </TabList>
             </Box>
-            <div style={{ marginLeft: "auto" }}>
-              <Button
-                variant="contained"
-                color="secondary"
-                style={{
-                  backgroundColor: "#F8F7FA",
-                  color: "black",
-                  marginRight: "8px",
-                  "&:hover": { boxShadow: `0 0 10px #F8F7FA` },
-                }}
-                onClick={handleCancel}
-              >
-                Cancel
-              </Button>
-              <Button
-                variant="contained"
-                color="primary"
-                style={{
-                  textTransform: "none",
-                  "&:hover": {
-                    boxShadow: `0 0 10px ${theme.palette.primary.main}`,
-                  },
-                }}
-              >
-                Confirm Ticket
-              </Button>
-            </div>
-          </div>
-        </Paper>
-      </Container>
+            <TabPanel value="1"><TicketsCard/></TabPanel>
+            <TabPanel value="2">Item Two</TabPanel>
+            <TabPanel value="3">Item Three</TabPanel>
+          </TabContext>
+          </Grid>
 
-      <Dialog
-        open={openCancelDialog}
-        onClose={handleCloseCancelDialog}
-        aria-labelledby="cancel-dialog-title"
-        aria-describedby="cancel-dialog-description"
-        style={{ padding: "5px" }}
-      >
-        <DialogContent style={{ textAlign: "center" }}>
-          <DialogContentText
-            style={{ color: "black" }}
-            id="cancel-dialog-description"
-          >
-            Do you want to discard?
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions style={{ justifyContent: "center" }}>
-          <Button
-            onClick={handleCloseCancelDialog}
-            style={{
-              border: "1px solid #0043b0",
-              borderRadius: "30px",
-              width: "100px",
-              height: "30px",
-              fontSize: "12px",
-              marginRight: "12px",
-              color: "#0043b0",
-              "&:hover": { color: "white", backgroundColor: "#005cf2" },
-            }}
-          >
-            Cancel
-          </Button>
-          <Button
-            onClick={handleCloseCancelDialog}
-            style={{
-              border: "1px solid #0043b0",
-              borderRadius: "30px",
-              width: "100px",
-              height: "30px",
-              fontSize: "12px",
-              marginRight: "12px",
-              color: "#FFFFFF",
-              backgroundColor: "#005cf2",
-              "&:hover": {
-                color: "#000000",
-                backgroundColor: "#005cf2",
-              },
-            }}
-            autoFocus
-          >
-            Discard
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </div>
+         
+        {/* </Box> */}
+      </Grid>
+      <Grid item md={2} xs={12} >
+          <Button variant="contained">Create Ticket</Button>
+          </Grid>
+    </Grid>
+    </Box>
   );
 };
-
-export default Tickets;
+ 
+export default TicketsPage;
