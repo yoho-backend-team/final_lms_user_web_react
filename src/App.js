e6600f7807122e44cf2b0b60a95f526e24231f80
Mohammed Thasthakir <mohammed.mt392@gmail.com>
2024-05-20 16:19:47 +0530

    thasthakir commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useSelector } from 'react-redux';
 import {useEffect} from 'react'
 import { ThemeProvider } from '@mui/material/styles';
 import { CssBaseline, StyledEngineProvider } from '@mui/material';
-
+import './App.css'
 // routing
 import Routes from './routes';
 
