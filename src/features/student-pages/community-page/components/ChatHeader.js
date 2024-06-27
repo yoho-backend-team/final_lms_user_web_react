2f3b757e0bd7fde2ef23d3650b99abc495c19861
Thiyagu110720 <163628089+Thiyagu110720@users.noreply.github.com>
2024-06-27 10:56:17 +0530

    student community

---
+++ src/features/student-pages/community-page/components/ChatHeader.js
@@ -0,0 +1,139 @@
+import React, { useState } from 'react';
+import { Box, Grid, Avatar, IconButton, Typography, AvatarGroup, Menu, MenuItem, Dialog, DialogTitle, DialogContent, DialogActions, Button, FormControl, FormControlLabel, RadioGroup, Radio, Tab, Tabs } from '@mui/material';
+import ExpandMoreRoundedIcon from '@mui/icons-material/ExpandMoreRounded';
+import { CommunityUser1, CommunityUser2, CommunityUser3 } from 'utils/images';
+import CallIcon from 'assets/icons/callIcon';
+import SearchIcon from 'assets/icons/searchIcon';
+import MuteNotificationModel from './Models/MuteNotification';
+import ReportModel from './Models/ReportDialog';
+import AddWallpaper from './Models/WallPaperModel';
+import MediaModel from './Models/Media';
+
+const ChatHeader = ({ currentChat }) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const [muteOpen, setMuteOpen] = useState(false);
+  const [reportOpen, setReportOpen] = useState(false);
+  const [wallpaperOpen, setWallpaperOpen] = useState(false);
+  const [mediaOpen, setMediaOpen] = useState(false);
+  const [mediaTab, setMediaTab] = useState(0);
+
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleMuteOpen = () => {
+    setMuteOpen(true);
+    handleMenuClose();
+  };
+
+  const handleReportOpen = () => {
+    setReportOpen(true);
+    handleMenuClose();
+  };
+
+  const handleWallpaperOpen = () => {
+    setWallpaperOpen(true);
+    handleMenuClose();
+  };
+
+  const handleMediaOpen = () => {
+    setMediaOpen(true);
+    handleMenuClose();
+  };
+
+  
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '16px',
+        // borderBottom: '1px solid #C3C3C3',
+      }}
+    >
+      <Grid container alignItems="center" spacing={2}>
+        <Grid item>
+          <Avatar sx={{ width: 40, height: 40 }} src={currentChat.image} variant='square' />
+        </Grid>
+        <Grid item>
+          <Typography variant="h6" sx={{ fontWeight: 700 }}>
+            {currentChat.batch_name}
+          </Typography>
+        </Grid>
+      </Grid>
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <AvatarGroup max={3}>
+          <Avatar alt="User 1" src={CommunityUser1} />
+          <Avatar alt="User 2" src={CommunityUser2} />
+          <Avatar alt="User 3" src={CommunityUser3} />
+        </AvatarGroup>
+        <IconButton>
+          <CallIcon />
+        </IconButton>
+        <IconButton>
+          <SearchIcon />
+        </IconButton>
+        <IconButton onClick={handleMenuOpen} sx={{ backgroundColor: open ? "#0D6EFD" : "white" }} >
+          <ExpandMoreRoundedIcon sx={{ color: open ? 'white' : '#130F26', cursor: "pointer" }} />
+        </IconButton>
+        <Menu
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleMenuClose}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'right',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          PaperProps={{
+            elevation: 0,
+            sx: {
+              padding: '24px 21px 44px 21px',
+              borderRadius: "28px",
+              border: "1px solid #DCDCDC",
+              boxShadow: "0px 4px 54px 0px rgba(0, 0, 0, 0.25)",
+              '& .MuiMenuItem-root': {
+                padding: '10px',
+                fontSize: "14px",
+                fontWeight: 500,
+                lineHeight: "28px",
+                color: "black",
+                display: "flex",
+                justifyItems: "center",
+                alignItems: "center",
+                '&:hover': {
+                  backgroundColor: '#0D6EFD',
+                  color: '#FFFFFF',
+                },
+              }
+            }
+          }}
+        >
+          <MenuItem onClick={handleMuteOpen}>Mute Notification</MenuItem>
+          <MenuItem onClick={handleReportOpen}>Report</MenuItem>
+          <MenuItem onClick={handleWallpaperOpen}>Wallpaper</MenuItem>
+          <MenuItem onClick={handleMediaOpen}>Group Media</MenuItem>
+        </Menu>
+      </Box>
+
+      <MuteNotificationModel open={muteOpen} setMuteOpen={setMuteOpen} />
+      <ReportModel open={reportOpen} setReportOpen={setReportOpen} />
+      <AddWallpaper open={wallpaperOpen} setWallpaperOpen={setWallpaperOpen} />
+      <MediaModel open={mediaOpen} setMediaOpen={setMediaOpen} />
+
+    </Box>
+  );
+};
+
+export default ChatHeader;
--- src/views/student-pages/community-page/index.js
+++ src/views/student-pages/community-page/index.js
@@ -1,11 +1,43 @@
-import React from 'react'
-import CommunityPageView from 'features/student-pages/community-page/components/Community/Community'
-const CommunityPage = () => {
-  return (
-    <div>
-      <CommunityPageView/>
-    </div>
-  )
-}
+import { Box, Grid } from "@mui/material";
+import { JavaCourseImage, SqlCourseImage } from "utils/images";
+import SideBar from "features/student-pages/community-page/components/Sidebar";
+import Chat from "features/student-pages/community-page/components/Chat";
+import PushPinOutlinedIcon from '@mui/icons-material/PushPinOutlined';
+import DoneAllOutlinedIcon from '@mui/icons-material/DoneAllOutlined';
+import { useState } from "react";
 
-export default CommunityPage
+const Batches = [
+  { batch_name: "Java Class", image: JavaCourseImage , id: "1",chat:<PushPinOutlinedIcon sx={{rotate:"35deg"}}  />,date:"5:14 pm",last_message : "Haha oh man" },
+  { batch_name: "SQL", image: SqlCourseImage , id: "2",chat : <DoneAllOutlinedIcon sx={{color:"#2361FF"}} />,date:"7:38 am",last_message:"Haha that's terrifying 😂" },
+];
+
+const CommunityPage = () => {
+  const [currentChat,setCurrentChat] = useState(null)
+  return (
+    <Box sx={{ height: "100vh", display: "flex", justifyContent: "center", alignItems: "center", padding: "40px" }}>
+      <Box
+        sx={{
+          backgroundColor: "#FFFFFF",
+          borderRadius: "18px",
+          border: "1px solid #C3C3C3",
+          boxShadow: "0px 0px 64px 0px rgba(0, 0, 0, 0.10)",
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <Grid container sx={{ flex: 1 }}>
+          <Grid item xs={4}>
+            <SideBar communities={Batches} currentChat={currentChat} setCurrentChat={setCurrentChat} />
+          </Grid>
+          <Grid item xs={8}>
+            <Chat currentChat={currentChat} setCurrentChat={setCurrentChat} />
+          </Grid>
+        </Grid>
+      </Box>
+    </Box>
+  );
+};
+
+export default CommunityPage;
