4eb7030eb46fa8e739f3d65bb6ba413e85637160
YOHOTECHNOLOGIESPVTLTD <developers.yoho@gmail.com>
2024-07-04 12:59:18 +0530

    add token in header

--- src/api/index.js
+++ src/api/index.js
@@ -3,15 +3,18 @@ import HTTP_END_POINTS from "./http_end_points";
 
 class Client {
   Student = {
-    login: (data) => httpClient.post(HTTP_END_POINTS.Student.auth.login, data),
-    verifyOtp : (data) => httpClient.post(HTTP_END_POINTS.Student.auth.verify_otp,data),
+    login: (data) => httpClient.post(HTTP_END_POINTS.Student.auth.login, data,"student"),
+    verifyOtp : (data) => httpClient.post(HTTP_END_POINTS.Student.auth.verify_otp,data,"student"),
     course : {
-      get : (params) => httpClient.get(HTTP_END_POINTS.Student.course.get,params)
+      get : (params) => httpClient.get(HTTP_END_POINTS.Student.course.get,params,"student")
     },
     class : {
-      get : (params) => httpClient.get(HTTP_END_POINTS.Student.class.get,params)
+      get : (params) => httpClient.get(HTTP_END_POINTS.Student.class.get,params,"student")
     },
-    attendance : (params) => httpClient.get(HTTP_END_POINTS.Student.attendance,params)
+    attendance : (params) => httpClient.get(HTTP_END_POINTS.Student.attendance,params,"student"),
+    community : {
+      get : (params) => httpClient.get(HTTP_END_POINTS.Student.community.get,params,"student")
+    }
   };
 
   common = {
@@ -22,31 +25,31 @@ class Client {
   }
 
   Instructor = {
-    login: (data) => httpClient.post(HTTP_END_POINTS.Instructor.auth.login, data),
-    verifyOtp: (data) => httpClient.post(HTTP_END_POINTS.Instructor.auth.verify_otp, data),
-    log_out : (data) => httpClient.post(HTTP_END_POINTS.Instructor.auth.log_out,data),
+    login: (data) => httpClient.post(HTTP_END_POINTS.Instructor.auth.login, data,'instructor'),
+    verifyOtp: (data) => httpClient.post(HTTP_END_POINTS.Instructor.auth.verify_otp, data,'instructor'),
+    log_out : (data) => httpClient.post(HTTP_END_POINTS.Instructor.auth.log_out,data,'instructor'),
     attendance: {
-      get: (params) => httpClient.get(`${HTTP_END_POINTS.Instructor.attendance.get}${params.userId}`),
-      get_class_attendance : (data) => httpClient.get(HTTP_END_POINTS.Instructor.attendance.class_attendance+data.classId),
-      update : (data) => httpClient.update(HTTP_END_POINTS.Instructor.attendance.class_attendance+data.uuid,data)
+      get: (params) => httpClient.get(`${HTTP_END_POINTS.Instructor.attendance.get}${params.userId}`,params,'instructor'),
+      get_class_attendance : (data) => httpClient.get(HTTP_END_POINTS.Instructor.attendance.class_attendance+data.classId,data,'instructor'),
+      update : (data) => httpClient.update(HTTP_END_POINTS.Instructor.attendance.class_attendance+data.uuid,data,'instructor')
     },
     course : {
-      get : (params) => httpClient.get(HTTP_END_POINTS.Instructor.course.get,params)
+      get : (params) => httpClient.get(HTTP_END_POINTS.Instructor.course.get,params,'instructor')
     },
     class : {
-      get : (params) => httpClient.get(HTTP_END_POINTS.Instructor.class.get,params),
-      getWithId : (params) => httpClient.get(HTTP_END_POINTS.Instructor.class.getwithId+params.course,params),
-      update : (data) => httpClient.update(HTTP_END_POINTS.Instructor.class.update+data?.uuid,data)
+      get : (params) => httpClient.get(HTTP_END_POINTS.Instructor.class.get,params,'instructor'),
+      getWithId : (params) => httpClient.get(HTTP_END_POINTS.Instructor.class.getwithId+params.course,params,'instructor'),
+      update : (data) => httpClient.update(HTTP_END_POINTS.Instructor.class.update+data?.uuid,data,'instructor')
     },
     community : {
-      get : () => httpClient.get(HTTP_END_POINTS.Instructor.community.get)
+      get : () => httpClient.get(HTTP_END_POINTS.Instructor.community.get,'instructor')
     },
     payment : {
-      get : () => httpClient.get(HTTP_END_POINTS.Instructor.payments.getSalaries)
+      get : () => httpClient.get(HTTP_END_POINTS.Instructor.payments.getSalaries,'instructor')
     },
     ticket : {
-      create : (data) => httpClient.post(HTTP_END_POINTS.Instructor.ticket.create,data),
-      get : (params) => httpClient.get(HTTP_END_POINTS.Instructor.ticket.get,params)
+      create : (data) => httpClient.post(HTTP_END_POINTS.Instructor.ticket.create,data,'instructor'),
+      get : (params) => httpClient.get(HTTP_END_POINTS.Instructor.ticket.get,params,'instructor')
     }
   };
 }
--- src/views/student-pages/community-page/index.js
+++ src/views/student-pages/community-page/index.js
@@ -4,7 +4,11 @@ import SideBar from "features/student-pages/community-page/components/Sidebar";
 import Chat from "features/student-pages/community-page/components/Chat";
 import PushPinOutlinedIcon from '@mui/icons-material/PushPinOutlined';
 import DoneAllOutlinedIcon from '@mui/icons-material/DoneAllOutlined';
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useDispatch,useSelector } from "react-redux";
+import { getAllStudentCommunities } from "features/student-pages/community-page/redux/thunks";
+import { selectStudentCommunity,selectLoading } from "features/student-pages/community-page/redux/selectors";
+import toast from "react-hot-toast";
 
 const Batches = [
   { batch_name: "Java Class", image: JavaCourseImage , id: "1",chat:<PushPinOutlinedIcon sx={{rotate:"35deg"}}  />,date:"5:14 pm",last_message : "Haha oh man" },
@@ -12,7 +16,21 @@ const Batches = [
 ];
 
 const CommunityPage = () => {
+  const dispatch = useDispatch()
+  const communities = useSelector(selectStudentCommunity)
   const [currentChat,setCurrentChat] = useState(null)
+
+  useEffect(()=>{
+    const getCommunities = async () => {
+        try{
+        dispatch(getAllStudentCommunities())
+        }catch(error){
+          toast.error(error?.response?.data?.message)
+        }
+    }
+    getCommunities()
+  },[dispatch])
+  console.log(communities,"communites")
   return (
     <Box sx={{ height: "100vh", display: "flex", justifyContent: "center", alignItems: "center", padding: "40px" }}>
       <Box
@@ -29,7 +47,7 @@ const CommunityPage = () => {
       >
         <Grid container sx={{ flex: 1 }}>
           <Grid item xs={4}>
-            <SideBar communities={Batches} currentChat={currentChat} setCurrentChat={setCurrentChat} />
+            <SideBar communities={communities} currentChat={currentChat} setCurrentChat={setCurrentChat} />
           </Grid>
           <Grid item xs={8}>
             <Chat currentChat={currentChat} setCurrentChat={setCurrentChat} />
