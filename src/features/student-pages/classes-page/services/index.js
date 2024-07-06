7519d0bf5ac253648ece8a81d3e26513f9473dda
Thiyagu110720 <163628089+Thiyagu110720@users.noreply.github.com>
2024-07-06 10:05:23 +0530

    edit profile virtualcard

--- src/features/student-pages/classes-page/services/index.js
+++ src/features/student-pages/classes-page/services/index.js
@@ -9,3 +9,12 @@ export const getAllClasses = async (data) => {
       throw new Error(error?.message)
     }
 }
+
+export const getClassDetails = async (data) => {
+  try {
+  const response = await Client.Student.class.getWithId(data)
+  return response?.data  
+  } catch (error) {
+    throw new Error(error)
+  }
+}
