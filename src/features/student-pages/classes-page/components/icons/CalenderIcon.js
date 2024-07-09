b1822ef70975da16a24570bf817b701e461f3d65
Thiyagu110720 <163628089+Thiyagu110720@users.noreply.github.com>
2024-07-09 15:27:17 +0530

    attendancee

---
+++ src/features/student-pages/classes-page/components/icons/CalenderIcon.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const CalendarIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    fill="currentColor"
+    className="bi bi-calendar"
+    viewBox="0 0 16 16"
+  >
+    <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1zm2-3a1 1 0 0 0-1 1v1h12V2a1 1 0 0 0-1-1h-1v.5a.5.5 0 0 1-1 0V1H5v.5a.5.5 0 0 1-1 0V1H3z" />
+  </svg>
+);
+
+export default CalendarIcon;
