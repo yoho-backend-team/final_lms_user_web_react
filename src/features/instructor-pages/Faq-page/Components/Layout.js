58989a3397cca1e6f0741196db047a4448e4b09c
unknown <raajeskumar.yoho@gmail.com>
2025-01-28 12:35:12 +0530

    Profile UI COrrections

--- src/features/instructor-pages/Faq-page/Components/Layout.js
+++ src/features/instructor-pages/Faq-page/Components/Layout.js
@@ -3,8 +3,24 @@ import { Container, Grid } from '@mui/material';
 
 const Layout = ({ children }) => {
   return (
-    <Container sx={{padding:"100px"}}>
-      <Grid container spacing={3}>
+    <Container
+    disableGutters
+    maxWidth={false}
+      sx={{
+        backgroundColor: "#e1e8ed",
+        backgroundImage: "url(https://ssl.gstatic.com/support/content/images/static/homepage_header_background_v2.svg)",
+        backgroundPosition: "50%",
+        backgroundPosition: "top",
+        backgroundRepeat: "no-repeat",
+        height: "50rem", // Increased height for the background image
+        paddingTop: "120px",
+        paddingBottom: "120px",
+        minHeight: "130vh",
+        width: "100%",
+
+      }}
+    >
+      <Grid container spacing={-2}>
         {children}
       </Grid>
     </Container>
