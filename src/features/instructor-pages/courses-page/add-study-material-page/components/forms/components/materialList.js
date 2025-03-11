f755a96a9c404f18df313f8ffae0c881da70e897
YOHOTECHNOLOGIESPVTLTD <developers.yoho@gmail.com>
2025-03-11 18:12:06 +0530

    notes and material error clear

--- src/features/instructor-pages/courses-page/add-study-material-page/components/forms/components/materialList.js
+++ src/features/instructor-pages/courses-page/add-study-material-page/components/forms/components/materialList.js
@@ -14,7 +14,7 @@ const useStyles = makeStyles((theme) => ({
       flexDirection: "column",
       minHeight: "140px",
       alignItems: "center",
-      padding: theme.spacing(3),
+      padding: 3,
       borderRadius: "14px",
       cursor: "pointer",
       "&:hover": {
@@ -25,10 +25,10 @@ const useStyles = makeStyles((theme) => ({
       display: "none",
     },
     text: {
-      marginBottom: theme.spacing(1),
+      marginBottom: 1,
     },
     browseButton: {
-      marginTop: theme.spacing(2),
+      marginTop: 2,
     },
     helperText: {
       "& .MuiFormHelperText-root": {
