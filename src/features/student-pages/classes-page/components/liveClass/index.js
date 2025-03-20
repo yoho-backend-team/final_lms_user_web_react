038aa36d822eb2a07f346bb0257f1e7a4f21a8dd
YOHOTECHNOLOGIESPVTLTD <developers.yoho@gmail.com>
2025-03-20 10:54:42 +0530

    attendance and community pages

--- src/features/student-pages/classes-page/components/liveClass/index.js
+++ src/features/student-pages/classes-page/components/liveClass/index.js
@@ -14,9 +14,10 @@ const LiveClassList = ({ data, classType,group }) => {
   return (
     <Box sx={{ mt: 3, px: "40px" }}> 
        {data.length > 0 ? (
-      data.map((cls) => (
-        <>
+         <>
         <ClassCardHeader/>
+      {data.map((cls) => (
+       
         <ClassCard
           key={cls.id}
           cls={cls}
@@ -24,8 +25,7 @@ const LiveClassList = ({ data, classType,group }) => {
           type={classType}
           group={group}
         />
-        </>
-      ))
+      ))}</>
     ) : (
       <Box sx={{ py: 5,textAlign:"center" }}>
          <img src="https://cdni.iconscout.com/illustration/premium/thumb/employee-is-unable-to-find-sensitive-data-illustration-download-in-svg-png-gif-file-formats--no-found-misplaced-files-business-pack-illustrations-8062128.png" alt="No data available" style={{ maxWidth: '100%', height: '270px' }} />
